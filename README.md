0dc3c3e2c14431f976c1a9c5e14c11b402ed5158
Sasha Bagley <volkadav99@list.ru>
2022-12-13 10:40:29 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Домашние задания по теме «Препроцессор и макросы»
+
+   [Первый проект Задача 1. Препроцессорные директивы](Task_1_Preprocessor_directives/Task_1_Preprocessor_directives.cpp)                                     
+   [Второй проект Задача 2. Макрос SUB](Task_2_Macro_SUB/Task_2_Macro_SUB.cpp)
---
+++ Task_1_Preprocessor_directives/Task_1_Preprocessor_directives.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#define MODE 1
+
+
+#if defined(MODE)
+#if MODE == 1
+int add(int a, int b) {
+	return a + b;
+}
+#endif
+int main()
+{
+	int f = MODE;
+	if (f == 0) {
+		std::cout << "Работаю в режиме тренеровки" << std::endl;
+	}
+	else if (f == 1) {
+		std::cout << "Работаю в режиме тренеровки" << std::endl;
+		int num1 = 0, num2 = 0;
+		std::cout << "Введите число 1: ";
+		std::cin >> num1;
+		std::cout << "Введите число 2: ";
+		std::cin >> num2;
+		std::cout << "Результат сложения: " << add(num1, num2) << std::endl;
+	}
+	else {
+		std::cout << "Неизвестный режим.Завершение работы" << std::endl;
+	}
+}
+#else
+#error no MODE
+#endif
+
