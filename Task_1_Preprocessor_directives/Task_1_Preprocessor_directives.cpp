7ddfd1c104ac37ccc1f0cef26af97c8e0033061d
Sasha Bagley <volkadav99@list.ru>
2022-12-16 05:15:01 +0300

    update project 1

--- Task_1_Preprocessor_directives/Task_1_Preprocessor_directives.cpp
+++ Task_1_Preprocessor_directives/Task_1_Preprocessor_directives.cpp
@@ -10,22 +10,20 @@ int add(int a, int b) {
 #endif
 int main()
 {
-	int f = MODE;
-	if (f == 0) {
-		std::cout << "Работаю в режиме тренеровки" << std::endl;
-	}
-	else if (f == 1) {
-		std::cout << "Работаю в режиме тренеровки" << std::endl;
-		int num1 = 0, num2 = 0;
-		std::cout << "Введите число 1: ";
-		std::cin >> num1;
-		std::cout << "Введите число 2: ";
-		std::cin >> num2;
-		std::cout << "Результат сложения: " << add(num1, num2) << std::endl;
-	}
-	else {
-		std::cout << "Неизвестный режим.Завершение работы" << std::endl;
-	}
+	setlocale(LC_ALL, "Rus");
+	#if MODE == 0
+	std::cout << "Работаю в режиме тренеровки" << std::endl;
+	#elif MODE == 1
+	std::cout << "Работаю в режиме тренеровки" << std::endl;
+	int num1 = 0, num2 = 0;
+	std::cout << "Введите число 1: ";
+	std::cin >> num1;
+	std::cout << "Введите число 2: ";
+	std::cin >> num2;
+	std::cout << "Результат сложения: " << add(num1, num2) << std::endl;
+	#else
+	std::cout << "Неизвестный режим.Завершение работы" << std::endl;
+	#endif 
 }
 #else
 #error no MODE
